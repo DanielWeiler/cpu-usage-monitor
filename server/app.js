e9f6ac0c908901fa23adfd3211359f15f062a8ee
Daniel Weiler <danielweiler78@gmail.com>
2022-06-14 12:54:40 +0300

    Initialize project frontend and backend

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import express, { json, urlencoded } from 'express'
+import NotFound from 'http-errors'
+import cpuDataRouter from './routes/cpu-data.route.js'
+
+const app = express()
+app.use(json())
+app.use(urlencoded({ extended: false }))
+
+app.use('/api/cpu-data', cpuDataRouter)
+
+app.use((req, res, next) => {
+  next(NotFound())
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    status: err.status || 500,
+    message: err.message
+  })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`))
